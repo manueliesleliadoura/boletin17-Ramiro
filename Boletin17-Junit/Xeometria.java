ee5e014fc361597af279d005f9287f5c6a15d9c7
JOTAJJ <ramirovquezfdez93@gmail.com>
2017-05-08 12:35:59 +0200

    Xeometria

---
+++ Boletin17-Junit/Xeometria.java
@@ -0,0 +1,29 @@
+package Boletin17;
+
+public class Xeometría {
+
+	public int perimetroCadrado(int lado) {
+		int perimetro;
+
+		perimetro = lado * 4;
+
+		return perimetro;
+	}
+
+	public int areaCadrado(int lado) {
+		int area;
+
+		area = lado * lado;
+
+		return area;
+
+	}
+
+	public double teoremaPitagoras(int catetoA, int catetoB) {
+		double hipotenusaC;
+
+		hipotenusaC = ((catetoA * catetoA) + (catetoB * catetoB));
+
+		return hipotenusaC;
+	}
+}
