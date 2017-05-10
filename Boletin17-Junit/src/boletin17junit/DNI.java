f05291f23b2008ed29a41fac578126bde1a9d603
JOTAJJ <ramirovquezfdez93@gmail.com>
2017-05-10 11:45:00 +0200

    Proba4

--- Boletin17-Junit/src/boletin17junit/DNI.java
+++ Boletin17-Junit/src/boletin17junit/DNI.java
@@ -2,4 +2,14 @@ package boletin17junit;
 
 public class DNI {
 
+	/* Genrar número aleatorio del DNI */
+	int numeroAleatorio = (int) (Math.random() * 25 + 1);
+
+	/* Cálculo de letra del DNI */
+	char calculaLetra(int dni) {
+		String juegoCaracteres = "TRWAGMYFPDXBNJZSQVHLCKE";
+		int modulo = dni % 23;
+		char letra = juegoCaracteres.charAt(modulo);
+		return letra;
+	}
 }
