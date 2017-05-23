a1ea50058ccd8b6de0a704879e75bdcb3d9fdc64
JOTAJJ <ramirovquezfdez93@gmail.com>
2017-05-23 23:47:27 +0200

    Xeometria-completo

--- Boletin17-Junit/src/boletin17junit/Xeometria.java
+++ Boletin17-Junit/src/boletin17junit/Xeometria.java
@@ -4,31 +4,78 @@ import java.lang.Math;
 
 public class Xeometria {
 
+	/**
+	 * Recibe un valor para lado y devuelve perimetro
+	 * 
+	 * @param lado
+	 * @return perimetro
+	 */
 	public int perimetroCadrado(int lado) {
 		int perimetro;
+
+		// esta hipotetica operacion la tendremos en cuenta para dar un
+		// resultado negativo
+		if (lado <= 0)
+			return -1;
 
 		perimetro = lado * 4;
 
 		return perimetro;
 	}
 
+	/**
+	 * Recibe un valor para lado y devuelve area
+	 * 
+	 * @param lado
+	 * @return area
+	 */
 	public int areaCadrado(int lado) {
 		int area;
 
-		area = lado * lado;
+		// mismo caso de arriba
+		if (lado <= 0)
+			return -1;
+
+		area = (lado * lado);
 
 		return area;
 
 	}
 
+	/**
+	 * Recibe valores para los catetos y devuelve la hipotenusa
+	 * 
+	 * @param catetoA
+	 * @param catetoB
+	 * @return hipotenusa
+	 */
+
 	public double teoremaPitagoras(int catetoA, int catetoB) {
+
+		// mismo procedimiento que arriba, desechar aquellas operaciones que
+		// no se puedan hacer
+		if (catetoA <= 0 || catetoB <= 0) {
+			return -1;
+		}
 
 		/* Me ahorro poner la variable hipotenusa */
 		return Math.sqrt((catetoA * catetoA) + (catetoB * catetoB));
 	}
 
+	/**
+	 * Recibe valores para la hipotenusa y uno de los catetos
+	 * 
+	 * @param catetoA
+	 * @param hipotenusaC
+	 * @return catetoB
+	 */
+
 	public double teoremaPitagoras(double catetoA, double hipotenusaC) {
 
+		// desechar las operaciones que no tienen sentido
+		if (catetoA <= 0 || hipotenusaC <= 0) {
+			return -1;
+		}
 		/*
 		 * Lo mismo que arriba, pero en este caso me ahorro la variable cateto
 		 */
--- Boletin17-Junit/src/boletin17junit/XeometriaTest.java
+++ Boletin17-Junit/src/boletin17junit/XeometriaTest.java
@@ -1,34 +1,24 @@
 package boletin17junit;
 
 import static org.junit.Assert.*;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class XeometriaTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
+	Xeometria xeometria = new Xeometria();
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
+	@Test
+	public void testperimetroCadrado() {
+		assertEquals(xeometria.perimetroCadrado(1), 4);
+		assertEquals(xeometria.perimetroCadrado(10), 40);
 
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testareaCadrado() {
+		assertEquals(xeometria.perimetroCadrado(2), 4);
+		assertEquals(xeometria.perimetroCadrado(10), 100);
+
 	}
 
 }
