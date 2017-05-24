0aba3c8c914c6307ee0a2d92ffe1057749c30555
JOTAJJ <ramirovquezfdez93@gmail.com>
2017-05-24 11:15:57 +0200

    Xeometria-Hecho

--- Boletin17-Junit/src/boletin17junit/XeometriaTest.java
+++ Boletin17-Junit/src/boletin17junit/XeometriaTest.java
@@ -11,6 +11,7 @@ public class XeometriaTest {
 	public void testperimetroCadrado() {
 		assertEquals(xeometria.perimetroCadrado(1), 4);
 		assertEquals(xeometria.perimetroCadrado(10), 40);
+		assertEquals(xeometria.perimetroCadrado(-1), -1);
 
 	}
 
@@ -18,6 +19,7 @@ public class XeometriaTest {
 	public void testareaCadrado() {
 		assertEquals(xeometria.areaCadrado(2), 4);
 		assertEquals(xeometria.areaCadrado(10), 100);
+		assertEquals(xeometria.areaCadrado(-1), -1);
 
 	}
 
@@ -25,6 +27,7 @@ public class XeometriaTest {
 	public void testteoremaPitagoras() {
 		equals(xeometria.teoremaPitagoras(1, 1));
 		equals(xeometria.teoremaPitagoras(10, 10));
+		equals(xeometria.teoremaPitagoras(-10, -10));
 
 	}
 
@@ -32,6 +35,7 @@ public class XeometriaTest {
 	public void testteoremaPitagorashp() {
 		equals(xeometria.teoremaPitagorashp(1, 1));
 		equals(xeometria.teoremaPitagorashp(10, 10));
+		equals(xeometria.teoremaPitagoras(-10, -10));
 
 	}
 
