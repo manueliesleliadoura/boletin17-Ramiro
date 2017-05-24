7472ec140ad882577eae0f09d59a8fac541d144a
JOTAJJ <ramirovquezfdez93@gmail.com>
2017-05-24 10:33:25 +0200

    Mitad-hecho

--- Boletin17-Junit/src/boletin17junit/DNITest.java
+++ Boletin17-Junit/src/boletin17junit/DNITest.java
@@ -5,17 +5,25 @@ import org.junit.Test;
 
 public class DNITest {
 
+	DNI dni = new DNI();
+
 	@Test
 	public void testeValido() {
-		
-		
+
+		equals(dni.eValido("53486688G"));
+		equals(dni.eValido("00000000k"));
+		equals(dni.eValido("000000007K"));
+		equals(dni.eValido("00000000kK"));
 
 	}
 
 	@Test
 	public void testcalculaLetra() {
-		
-		
+
+		equals(dni.eValido("53486688"));
+		equals(dni.eValido("00000000"));
+		equals(dni.eValido("000000007"));
+		equals(dni.eValido("00000000k"));
 
 	}
 
